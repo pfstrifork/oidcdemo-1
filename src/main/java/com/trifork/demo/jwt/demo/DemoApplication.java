7f8ece97e1f750069227c28ddd6339d8e2d6c7a7
Peter Flintholm <pfs@trifork.com>
2019-09-22 11:38:47 +0200

    Init

---
+++ src/main/java/com/trifork/demo/jwt/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.trifork.demo.jwt.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		logger.info("Hello world!");
+	}
+}
